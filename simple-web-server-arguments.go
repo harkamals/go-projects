b9d6cb667f37bd759da2e80500e09a2c02cf7ad3
HK <harkamals@gmail.com>
2017-08-09 00:48:04 +0800

    Simple web server

---
+++ simple-web-server-arguments.go
@@ -0,0 +1,27 @@
+// MIT License
+// Copyright (c) 2017 @harkamals
+
+// Simple web server-with-arguments
+// Usage: go run simple-web-server-arguments.go -port 5051 -path /mypath
+
+package main
+
+import (
+	"os"
+	"net/http"
+	"flag"
+)
+
+func main() {
+	var dir string
+	port := flag.String("port", "3000", "port to serve HTTP on")
+	path := flag.String("path", "", "path to serve")
+	flag.Parse()
+
+	if *path == "" {
+		dir, _ = os.Getwd()
+	} else {
+		dir = *path
+	}
+	http.ListenAndServe(":"+*port, http.FileServer(http.Dir(dir)))
+}
