2c9e55e4927d6f7150a770c4757a235628795640
HK <harkamals@gmail.com>
2017-08-09 23:27:32 +0800

    EoD

---
+++ functions.go
@@ -0,0 +1,32 @@
+// MIT License
+// Copyright (c) 2017 @harkamals
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func printer(format string, messages ...string) (count int, e error) {
+	defer fmt.Printf("Messages: %d\n", len(messages))
+
+	_, e = fmt.Printf(format, messages)
+	count = len(messages)
+
+	return
+}
+
+func main() {
+
+	count, err := printer(
+		"%s\n",
+		"Hello", " World!", "How is the weather")
+
+	if err == nil {
+		fmt.Printf("Count: %d", count)
+	} else {
+		defer os.Exit(-1)
+	}
+
+}
