bbcbdaa3f3de983342697cd301b8faec7bf0257d
HK <harkamals@gmail.com>
2017-08-09 18:58:40 +0800

    Data types

---
+++ data-types.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	name := "Jason Bourne"
+	age := 35
+
+	fmt.Println("Name:", name,"Age:", age)
+	fmt.Printf("Name: %v Age: %d\n", name, age)
+
+	// Tabular StdOut
+	fmt.Printf("%15v %4v\n", name, age)
+	fmt.Printf("%15v %4v\n", name, age + rand.Intn(38))
+
+	// String
+	var hero = strings.Contains(name, "Bourne")
+	fmt.Printf("Hero: %t", hero)
+
+
+}
