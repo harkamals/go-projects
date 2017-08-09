6225a47b2c2ad53d515bdb561281e7911707e49d
HK <harkamals@gmail.com>
2017-08-09 20:50:44 +0800

    control-structure

--- control-structures.go
+++ control-structures.go
@@ -7,12 +7,20 @@ import (
 
 func main() {
 
-	// inline declaration
+	// if-else with inline declaration
 	if i, err := fmt.Printf("random string\n"); err != nil {
 		fmt.Println("Error occurred")
 		os.Exit(1)
+	} else if i > 100 {
+		fmt.Printf("Bytes more than expected: %d", i)
 	} else {
-		fmt.Printf("Bytes returned: %d", i)
+		fmt.Printf("Bytes returned: %d\n", i)
+	}
+
+	// simple error check
+	_, err := fmt.Printf("another string\n")
+	if err != nil {
+		os.Exit(-1)
 	}
 
 }
