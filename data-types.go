d7e18df0ef0f8b08273261dc8b3236c17d490e0e
HK <harkamals@gmail.com>
2017-08-09 18:59:46 +0800

    Data types

--- data-types.go
+++ data-types.go
@@ -12,16 +12,15 @@ func main() {
 	name := "Jason Bourne"
 	age := 35
 
-	fmt.Println("Name:", name,"Age:", age)
+	fmt.Println("Name:", name, "Age:", age)
 	fmt.Printf("Name: %v Age: %d\n", name, age)
 
 	// Tabular StdOut
 	fmt.Printf("%15v %4v\n", name, age)
-	fmt.Printf("%15v %4v\n", name, age + rand.Intn(38))
+	fmt.Printf("%15v %4v\n", name, age+rand.Intn(age))
 
 	// String
 	var hero = strings.Contains(name, "Bourne")
 	fmt.Printf("Hero: %t", hero)
-
 
 }
