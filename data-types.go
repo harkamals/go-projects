ea1471bc490a38aa6f6d6d2f3ad59e7fc91f2eea
HK <harkamals@gmail.com>
2017-08-12 17:58:43 +0800

    Types

--- data-types.go
+++ data-types.go
@@ -12,33 +12,6 @@ import (
 	"strings"
 )
 
-type webPage struct {
-	url  string
-	body []byte
-	err  error
-}
-
-func (w *webPage) get() {
-
-	resp, err := http.Get(w.url)
-	if err != nil {
-		w.err = err
-		return
-	}
-
-	defer resp.Body.Close()
-
-	w.body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		w.err = err
-	}
-
-}
-
-func (w *webPage) isOK() bool {
-	return w.err == nil
-}
-
 func main() {
 	fmt.Println("Hello World")
 
@@ -75,15 +48,5 @@ func main() {
 	// Array - fixed length
 	words4 := [5]string{"the", "quick", "brown", "fox"}
 	fmt.Println(words4)
-
-	// Get webPage
-	w := &webPage{url: "http://g213oogle.com"}
-	w.get()
-	fmt.Printf("Url:%s Err:%s Len:%d\n", w.url, w.err, len(w.body))
-	if w.isOK() {
-		fmt.Println("WebPage downloaded")
-	} else {
-		fmt.Println("Error while downloading")
-	}
 
 }
---
+++ types.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// User defined type
+type webPage struct {
+	url  string
+	body []byte
+	err  error
+}
+
+func (w *webPage) get() {
+
+	resp, err := http.Get(w.url)
+	if err != nil {
+		w.err = err
+		return
+	}
+
+	defer resp.Body.Close()
+
+	w.body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.err = err
+	}
+
+}
+
+func (w *webPage) isOK() bool {
+	return w.err == nil
+}
+
+// User defined type (or as an alias to built-in type)
+type SummableSlice []int
+
+// Why no *pointer here ?!
+func (s SummableSlice) sum() int {
+	sum := 0
+	for _, i := range s {
+		sum += i
+	}
+	return sum
+}
+
+func main() {
+
+	// Get webPage
+	//------------------------------------------------------
+	// Method -1
+	// w := &webPage{url: "http://google.com"}
+
+	// Method -2
+	// w := &webPage{}
+	//w.url = "http://google.com"
+
+	// Method -3
+	w := new(webPage)
+	w.url = "http://google.com"
+
+	w.get()
+	fmt.Printf("Url:%s Err:%s Len:%d\n", w.url, w.err, len(w.body))
+	if w.isOK() {
+		fmt.Println("WebPage downloaded")
+	} else {
+		fmt.Println("Error while downloading")
+	}
+
+	// Summable slice - method call
+	s := &SummableSlice{1, 2, 3}
+	fmt.Println(s.sum())
+
+}
