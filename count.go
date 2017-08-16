1a8c4580ff441f444efdfd830d83b062068232b8
HK <harkamals@gmail.com>
2017-08-16 23:30:47 +0800

    go-routine

--- count.go
+++ count.go
@@ -1,3 +1,6 @@
+// MIT License
+// Copyright (c) 2017 @harkamals
+
 package main
 
 import (
@@ -5,18 +8,30 @@ import (
 	"time"
 )
 
-func count() {
-	for i := 0; i <= 5; i++ {
-		fmt.Println(i)
-		time.Sleep(time.Millisecond * 1)
+func printCount(c chan int) {
+
+	num := 0
+	for num >= 0 {
+		num = <-c
+		fmt.Println(num, " <-")
 	}
+
 }
 
 func main() {
-	go count()
 
-	time.Sleep(time.Millisecond * 1)
-	fmt.Println("Counting machine")
-	time.Sleep(time.Millisecond * 5)
+	fmt.Println("- start counter -")
+
+	c := make(chan int)
+	a := []int{8, 7, 6, 5, 4, 3, 2, 1, 0, 8, -1}
+
+	go printCount(c)
+
+	for _, v := range a {
+		c <- v
+	}
+
+	time.Sleep(time.Millisecond)
+	fmt.Println("- the end -")
 
 }
