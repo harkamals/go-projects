341490f47caeeb16051c810670bf006332df25ce
HK <harkamals@gmail.com>
2017-09-10 01:11:11 +0800

    golang adventure

--- src/main.go
+++ src/main.go
@@ -6,6 +6,15 @@ import (
 
 func main() {
 
+	// Slice
+	// slice_1 := make([]int, 5)
+	slice_1 := []int{1, 2, 3, 4, 5}
+	print_slice_stat(slice_1)
+
+	slice_2 := append(slice_1, 6, 7, 8)
+	print_slice_stat(slice_2)
+
+	// Calculator
 	a := 9
 	b := 6
 
@@ -13,6 +22,14 @@ func main() {
 	calculate("-", a, b)
 	calculate("*", a, b)
 	calculate("/", a, b)
+
+}
+func print_slice_stat(slice []int) {
+	fmt.Printf(
+		"Capacity: %d, Len: %d, Value: %d\n",
+		cap(slice),
+		len(slice),
+		slice)
 
 }
 
